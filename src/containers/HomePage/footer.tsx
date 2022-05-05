3d6fe619509157c287c536e432adaa115a3648c5
ThisIsBond <joshisaurabh7788@gmail.com>
2022-05-06 00:14:19 +0530

    fixed footer

--- src/containers/HomePage/footer.tsx
+++ src/containers/HomePage/footer.tsx
@@ -1,21 +1,26 @@
-import { faArrowRight, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Marginer } from "../../components/marginer";
+import { SCREENS } from "../../components/responsive";
 import { ReactComponent as Arrow } from './arrowupright.svg';
 
 const MainContainer = styled.div`
-    max-height: 1000px;
-    min-height: 700px;
+    position: relative;
+    left: 0;
+    bottom: 0;
     background-color: #333333;
+    ${tw`
+        w-full
+        flex
+    `}
 `;
 const TopSectionContainer = styled.div`
     
     ${tw`
-        w-full
-        h-auto
         max-w-screen-2xl
         flex
         flex-wrap
@@ -23,15 +28,6 @@ const TopSectionContainer = styled.div`
         items-center
         overflow-hidden
     `};
-    @media (min-width: 1024){
-        height: 800;
-    };
-    @media (min-width: 800){
-        height: 900;
-    };
-    @media (min-width: 600){
-        height: 1000;
-    };
 `;
 
 const LeftContainer = styled.div`
@@ -132,14 +128,15 @@ const SustainabilityMenu = styled.ul`
 `;
 
 const SectionContainer = styled.div`
+    
     min-width: 220px;
     ${tw`
-        w-full
+        w-auto
         md:w-auto
         flex
         flex-col
         mr-2
-        md:mr-10
+        md:mr-5
         pl-10
         pr-10
         md:pl-3
@@ -148,6 +145,9 @@ const SectionContainer = styled.div`
         md:mt-0
         mb-8
     `};
+    @media(max-width: 920px){
+        min-width: 45%;
+    }
 `;
 
 const LinksList = styled.ul`
@@ -211,12 +211,247 @@ const AesopCopyright = styled.div`
         p-5
         text-lg
         text-white
+    `};
+`;
+
+const TabletViewMainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #333333;
+    ${tw`
+        
+        p-10
+        flex
+        flex-col
+    `};
+`;
+
+const TabletViewDescriptionContainer = styled.div`
+        height: inherit;
+        width: inherit;
+    ${tw`
+
+    `};
+`;
+
+const TabletViewCheckBoxWithDescription = styled.div`
+    ${tw`
+        flex
+        flex-row
+        items-start
+        mt-5
+    `};
+`;
+
+const TabletViewDescription = styled.p`
+    ${tw`
+        ml-2
+        text-white
+    `};
+`;
+
+const TabletViewCheckboxWrapper = styled.input`
+    margin-top: 5px;
+`;
+
+const TabletViewUnorderedListWrapper = styled.div`
+    ${tw`
+        flex
+        flex-row
+        flex-wrap
+        justify-between
     `}
-`
+`;
 
 
 export function Footer() {
 
+
+    const isTablet = useMediaQuery({ maxWidth: SCREENS.md });
+
+    if (isTablet) {
+        return (
+            <MainContainer>
+                <TabletViewMainContainer>
+                    <TabletViewDescriptionContainer>
+                        <FilledButton>
+                            <InputContainer type="text" placeholder="Email address" />
+                            <ButtonIcon>
+                                <FontAwesomeIcon icon={faArrowRight} color="#ffffff" />
+                            </ButtonIcon>
+                        </FilledButton>
+                    </TabletViewDescriptionContainer>
+                    <TabletViewCheckBoxWithDescription>
+                        <TabletViewCheckboxWrapper type="checkbox" />
+                        <TabletViewDescription>
+                            Subscribe to receive communications from Aesop about our products and services. By subscribing, you confirm you have read and accept our privacy policy.
+                        </TabletViewDescription>
+                    </TabletViewCheckBoxWithDescription>
+                    <TabletViewUnorderedListWrapper>
+                        <SectionContainer>
+                            <HeaderTitle>
+                                Order and support
+                            </HeaderTitle>
+                            <Separator />
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Contact us</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">FAQ</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Shipping</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Returns</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Order history</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Terms &amp; conditions</a>
+                                </ListItem>
+                            </LinksList>
+                        </SectionContainer>
+
+                        <SectionContainer>
+                            <HeaderTitle>
+                                Services
+                            </HeaderTitle>
+                            <Separator />
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Live assistance</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Corporate gifts</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Facial appointments</a>
+                                </ListItem>
+                            </LinksList>
+                        </SectionContainer>
+
+                        <SectionContainer>
+                            <HeaderTitle>
+                                Location preferences
+                            </HeaderTitle>
+                            <Separator />
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Shipping: &nbsp; </a>
+                                    <p className="underline" style={{ color: '#ffffff' }}>Hong Kong(S.A.R)</p>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Language: &nbsp; </a>
+                                    <p className="underline" style={{ color: '#ffffff' }}>English</p>
+                                </ListItem>
+                            </LinksList>
+                        </SectionContainer>
+
+                        <SectionContainer>
+                            <HeaderTitle>
+                                About
+                            </HeaderTitle>
+                            <Separator />
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Our story</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Foundation</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Careers</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Accessibility</a>
+                                </ListItem>
+                            </LinksList>
+                        </SectionContainer>
+
+                        <SectionContainer>
+                            <HeaderTitle>
+                                Social Media
+                            </HeaderTitle>
+                            <Separator />
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Instagram</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Tweeter</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">LinkedIn</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">WeChat</a>
+                                </ListItem>
+                            </LinksList>
+
+                            <LinksList>
+                                <ListItem>
+                                    <a href="#">Waibo</a>
+                                    <Arrow style={{ marginLeft: 5, fill: "#ffffff" }} />
+                                </ListItem>
+                            </LinksList>
+                        </SectionContainer>
+                    </TabletViewUnorderedListWrapper>
+                </TabletViewMainContainer>
+            </MainContainer>
+        )
+    }
 
     return (
         <MainContainer>
@@ -414,10 +649,6 @@ export function Footer() {
                     </RightContainerWrapper>
                 </RightContainer>
             </TopSectionContainer>
-            <Separator/>
-            <AesopCopyright>
-                &copy; Aesop
-            </AesopCopyright>
         </MainContainer>
     )
 }
